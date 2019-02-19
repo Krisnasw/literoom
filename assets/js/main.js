991190c782b398f9fcdd1c0cc2dfee498b309684
eastoni123 <ttoonnii321@gmail.com>
2019-02-19 07:50:17 +0700

    done all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+var galleryTop = new Swiper('#slideshow', {
+	spaceBetween: 0,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	loop:true,
+	pagination: {
+		el: '.number-image',
+		type:'fraction',
+	},
+});
+
+
 $(document).ready(function(){
 	// ONSCROLL
 	$(window).scroll(function(){
@@ -9,6 +23,13 @@ $(document).ready(function(){
 	});
 
 	// SLIDE MENU
-
+	$(".btn-menu").click(function(){
+		$(".menu").removeClass('menu-in');
+		$(".menu").addClass('menu-out');
+	});
+	$(".btn-close").click(function(){
+		$(".menu").removeClass('menu-out');
+		$(".menu").addClass('menu-in');
+	});
 
 });
